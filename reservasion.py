1096c7c69faea4a67abeaef5ed26fd23255b8508
Joanjajas <joanbeltranperis@gmail.com>
2023-10-19 00:07:01 +0200

    yss

--- reservasion.py
+++ reservasion.py
@@ -1,19 +1,28 @@
-from getpass import getpass
-import sys
-
 from playwright.sync_api import sync_playwright
-from date import get_day_of_week_spanish
 
 UPV_LOGIN_URL = "https://intranet.upv.es/"
 USERNAME = "20934366"
 PASSWORD = "cuswiw-sukti0-hehbEv"
 
 
-def run(playwright):
-    sport = input("Deporte: ").upper()
-    date = get_day_of_week_spanish(input("Fecha (dd/mm/yyyy): "))
-    time = input("Hora (hh:mm-hh:mm): ")
+class Reservation:
+    def __init__(self, sport, date, time, court):
+        self.sport = sport
+        self.date = date
+        self.time = time
+        self.court = court
 
+    def __str__(self):
+        return f"Reservada pista de {self.sport} el {self.date} de {self.time} en la pista {self.court}"
+
+
+RESERVATIONS = [
+    Reservation("PADEL", "Sábado 21/10/2023", "08:00-09:00", "PÁDEL EXTERIOR 2"),
+    Reservation("TENIS", "Martes 24/10/2023", "08:00-09:00", "TENIS 1"),
+]
+
+
+def run(playwright):
     # Create a new instance of chromium and open a new page
     chromium = playwright.chromium
     browser = chromium.launch(headless=False)
@@ -25,69 +34,57 @@ def run(playwright):
     # Log in
     login(page, USERNAME, PASSWORD)
 
-    # Log in and navigate to the grades page
+    # Navigate to the reservations page
     goto_revervations(page)
 
-    # Make the reservation
-    make_reservation(page, sport, date, time)
+    print("Reservando pistas...")
 
-    input("")
+    for reservation in RESERVATIONS:
+        make_reservation(page, reservation)
+        print(reservation)
 
 
-def make_reservation(page, sport, date, time):
-    print("Making reservation...")
-
+def make_reservation(page, reservation: Reservation):
     # Select the sport
-    page.locator(f"//select[@name='deporte']").select_option(sport)
+    page.locator(f"//select[@name='deporte']").select_option(reservation.sport)
 
     # Select the day
-    page.get_by_role("button", name=date).click()
+    page.get_by_role("button", name=reservation.date).click()
+
+    # Select the court
+    title = page.get_by_role("columnheader", name=reservation.court)
 
     # Select the time
-    title = page.get_by_role("columnheader", name="PÁDEL EXTERIOR 1")
-    tabla = title.locator("xpath=ancestor::table")
-    tabla.get_by_role("row", name=f"{time} Libre", exact=True).get_by_role(
+    tabla = title.locator("xpath=ancestor::table[@class='upv_listacolumnas']")
+    tabla.get_by_role("row", name=f"{reservation.time} Libre", exact=True).get_by_role(
         "link"
     ).click()
 
+    # Confirm the reservation
+    page.get_by_role("button", name="Cancel").click()
+
 
 def goto_revervations(page):
-    try:
-        print("Navigating to reservations page...")
+    print("Navegando a reservas")
 
-        # Enter intranet
-        page.locator("//div[@id='intranet']//a[2]").click()
+    # Enter intranet
+    page.locator("//div[@id='intranet']//a[2]").click()
 
-        # Enter reservation page
-        page.locator("//div[@id='subgrupo_1000']//table[@id='elemento_1001']").click()
-
-    except Exception as err:
-        print(f"Error ocurred while navigating to reservations: {err}", file=sys.stderr)
-        exit(1)
+    # Enter reservation page
+    page.locator("//div[@id='subgrupo_1000']//table[@id='elemento_1001']").click()
 
 
 def login(page, username, password):
-    print("Logging in...")
+    print("Iniciando sesión...")
 
-    try:
-        # Go to the login page
-        page.goto(UPV_LOGIN_URL)
+    # Go to the login page
+    page.goto(UPV_LOGIN_URL)
 
-        # Fill the login form and submit it
-        form = page.locator("form[name='alumno']")
-        form.locator("input[name='dni']").fill(username)
-        form.locator("input[name='clau']").fill(password)
-        form.locator("input[type='submit']").click()
-
-        # Check if the login was succesfull
-        assert page.title() == "Mi UPV"
-
-    except AssertionError:
-        print(
-            "Login failed. Make sure to use a valid username and password.",
-            file=sys.stderr,
-        )
-        exit(1)
+    # Fill the login form and submit it
+    form = page.locator("form[name='alumno']")
+    form.locator("input[name='dni']").fill(username)
+    form.locator("input[name='clau']").fill(password)
+    form.locator("input[type='submit']").click()
 
 
 if __name__ == "__main__":
