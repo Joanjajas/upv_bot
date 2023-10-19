35caf8689ad6178d6416784b8f028b54e31a504f
Joanjajas <joanbeltranperis@gmail.com>
2023-10-19 22:30:42 +0200

    feaw

---
+++ bot/logger.py
@@ -0,0 +1,13 @@
+import datetime
+import os
+
+
+def log(text: str, level: str = "INFO"):
+    current_time = datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
+    print(f"[{current_time} - {level}]: {text}")
+
+
+def create_log_file(filename: str):
+    if not os.path.exists("../logs"):
+        with open(filename, "w") as f:
+            f.write("")
