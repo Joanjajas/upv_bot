eb7fb47335ff91196469728931e49e3ff418eef9
Joanjajas <joanbeltranperis@gmail.com>
2023-10-19 22:30:15 +0200

    fsdf

--- logger.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-
-def log(text: str, level: str = "INFO"):
-    current_time = datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
-    print(f"[{current_time} - {level}]: {text}")
