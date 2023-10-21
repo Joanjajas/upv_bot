473fdfbdafcac1b9b565e4ae36221f33f5d4590c
Joanjajas <joanbeltranperis@gmail.com>
2023-10-21 13:11:27 +0200

    cr

--- setup.sh
+++ setup.sh
@@ -3,22 +3,44 @@
 script_dir=$(dirname "$(readlink -f "$0")")
 plist_file="$script_dir/reservation_bot.plist"
 
+# Install the necessary python dependencies
+/usr/bin/python3 -m pip install playwright
+/usr/bin/python3 -m playwright install chromium
+
+# Get user credentials
+while true; do
+	printf "\nIntroduce el usuario y contraseña de la intranet.\n"
+	read -r -p "DNI: " username
+	read -r -s -p "Contraseña: " password
+
+	credentials_script="$script_dir/check_credentials.py"
+
+	printf "\nComprobando que el usuario y la contraseña son correctos...\n"
+
+	if /usr/bin/python3 "$credentials_script" "$username" "$password"; then
+		break
+	else
+		printf "El usuario o la contraseña son incorrectos.\n"
+	fi
+done
+
+sed -i '' "s/USERNAME = \".*\"/USERNAME = \"$username\"/" "$script_dir/bot/bot.py"
+sed -i '' "s/PASSWORD = \".*\"/PASSWORD = \"$password\"/" "$script_dir/bot/bot.py"
+
+# Make sure to create the necessary directories and files
 if [ ! -d ~/bot_reservas ]; then
 	mkdir ~/bot_reservas
 fi
 
 if [ ! -f ~/bot_reservas/reservas.toml ]; then
-	cp "$script_dir/reservas.toml" ~/bot_reservas/ || touch ~/bot_reservas/reservas.toml
+	cp "$script_dir/reservas.toml" ~/bot_reservas/
 fi
 
 if [ ! -d /usr/local/reservation_bot ]; then
 	sudo cp -r "$script_dir/bot" /usr/local/reservation_bot
 fi
 
-/usr/bin/python3 -m pip install playwright
-/usr/bin/python3 -m playwright install chromium
-
-# if there is a previous version of the plist file, unload it
+# Create the launch agent
 if [ -f ~/Library/LaunchAgents/reservation_bot.plist ]; then
 	launchctl unload ~/Library/LaunchAgents/reservation_bot.plist
 fi
