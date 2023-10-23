7c533d25a5cf09e1d3c0d89aa798e2ec732c7823
Joanjajas <joanbeltranperis@gmail.com>
2023-10-23 22:58:38 +0200

    fsljkafe

--- scripts/setup.py
+++ scripts/setup.py
@@ -2,16 +2,13 @@ import platform
 import shutil
 import os
 import subprocess
-from getpass import getpass
 
-from playwright.sync_api import sync_playwright
-
-UPV_LOGIN_URL = "https://intranet.upv.es"
+from get_credentials import get_credentials
 
 
 def main():
     install_deps()
-    get_credentials()
+    username, password = get_credentials()
     install_script()
 
 
@@ -40,41 +37,6 @@ def run_command(command):
     except Exception as err:
         print("Error running command: ", err)
         exit(1)
-
-
-def get_credentials():
-    # Get intranet username and password
-    print("\nIntroduce your intranet username and password:")
-    username = input("Username: ")
-    password = getpass("Password: ")
-
-    # Check if the credentials are valid by trying to login in the intranet
-    print("Checking if the intranet username and password are valid")
-    with sync_playwright() as playwright:
-        chromium = playwright.chromium
-        browser = chromium.launch()
-        page = browser.new_page()
-
-        try:
-            # Navigate to login page
-            page.goto(UPV_LOGIN_URL)
-
-            # Fill login form and submit
-            form = page.locator("form[name='alumno']")
-            form.locator("input[name='dni']").fill(username)
-            form.locator("input[name='clau']").fill(password)
-            form.locator("input[type='submit']").click()
-
-            # Check if login was successful
-            assert page.title() == "Mi UPV"
-
-        except AssertionError:
-            print("The intranet username or password are not valid")
-            # exit(1)
-
-        except Exception as err:
-            print(err)
-            exit(1)
 
 
 def install_script():
