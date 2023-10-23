50133e03ce1c57b5c5ac19c943c99f13577d5950
Joanjajas <joanbeltranperis@gmail.com>
2023-10-23 21:49:21 +0200

    dsjklf

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,3 +1,6 @@
+import platform
+import shutil
+import os
 import subprocess
 from getpass import getpass
 
@@ -9,18 +12,52 @@ UPV_LOGIN_URL = "https://intranet.upv.es"
 def main():
     install_deps()
     get_credentials()
+    install_script()
 
 
 def install_deps():
     print("Installing dependencies...")
-    subprocess.run(["/usr/bin/python3", "-m", "pip", "install", "playwright"])
-    subprocess.run(["/usr/bin/python3", "-m", "playwright", "install", "chromium"])
-    subprocess.run(["/usr/bin/python3", "-m", "pip", "install", "toml"])
+
+    if platform.system() == "Darwin":
+        try:
+            run_command(["/usr/bin/python3", "-m", "pip", "install", "toml"])
+            run_command(["/usr/bin/python3", "-m", "pip", "install", "playwright"])
+            run_command(["/usr/bin/python3", "-m", "playwright", "install", "chromium"])
+
+        except Exception:
+            print("Unable to install dependencies, check your python installation")
+
+    if platform.system() == "Windows":
+        try:
+            run_command(["python", "-V"])
+            run_command(["pip", "-V"])
+
+        except Exception:
+            print("Python is not installed, installing it via winget")
+
+            try:
+                run_command(["winget", "install", "python"])
+
+            except Exception:
+                print("Unable to install python, please install it manually")
+                exit(1)
+
+        run_command(["pip", "install", "toml"])
+        run_command(["pip", "install", "playwright"])
+        run_command(["python", "-m", "playwright", "install", "chromium"])
+
+
+def run_command(command):
+    subprocess.run(
+        command,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
 
 
 def get_credentials():
     # Get intranet username and password
-    print("Introduce your intranet username and password:")
+    print("\nIntroduce your intranet username and password:")
     username = input("Username: ")
     password = getpass("Password: ")
 
@@ -46,11 +83,25 @@ def get_credentials():
 
         except AssertionError:
             print("The intranet username or password are not valid")
-            exit(1)
+            # exit(1)
 
         except Exception as err:
             print(err)
             exit(1)
+
+
+def install_script():
+    install_dir = "/usr/local/reservation_bot"
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(script_dir)
+
+    print(root_dir)
+    if platform.system() == "Darwin":
+        if os.path.exists(install_dir):
+            shutil.rmtree(install_dir)
+
+        print("Installing script...")
+        shutil.copytree(root_dir, install_dir)
 
 
 if __name__ == "__main__":
