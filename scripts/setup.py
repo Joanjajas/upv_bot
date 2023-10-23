53266e8d3403b76710cce6c2a6e858e2c681d2be
Joanjajas <joanbeltranperis@gmail.com>
2023-10-23 22:19:01 +0200

    sdfljksafe

--- scripts/setup.py
+++ scripts/setup.py
@@ -19,40 +19,27 @@ def install_deps():
     print("Installing dependencies...")
 
     if platform.system() == "Darwin":
-        try:
-            run_command(["/usr/bin/python3", "-m", "pip", "install", "toml"])
-            run_command(["/usr/bin/python3", "-m", "pip", "install", "playwright"])
-            run_command(["/usr/bin/python3", "-m", "playwright", "install", "chromium"])
-
-        except Exception:
-            print("Unable to install dependencies, check your python installation")
+        run_command(["/usr/bin/python3", "-m", "pip", "install", "toml"])
+        run_command(["/usr/bin/python3", "-m", "pip", "install", "dsfdsfdsf"])
+        run_command(["/usr/bin/python3", "-m", "playwright", "install", "chromium"])
 
     if platform.system() == "Windows":
-        try:
-            run_command(["python", "-V"])
-            run_command(["pip", "-V"])
-
-        except Exception:
-            print("Python is not installed, installing it via winget")
-
-            try:
-                run_command(["winget", "install", "python"])
-
-            except Exception:
-                print("Unable to install python, please install it manually")
-                exit(1)
-
         run_command(["pip", "install", "toml"])
         run_command(["pip", "install", "playwright"])
         run_command(["python", "-m", "playwright", "install", "chromium"])
 
 
 def run_command(command):
-    subprocess.run(
-        command,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-    )
+    try:
+        subprocess.run(
+            command,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+
+    except Exception as err:
+        print("Error running command: ", err)
+        exit(1)
 
 
 def get_credentials():
@@ -61,7 +48,7 @@ def get_credentials():
     username = input("Username: ")
     password = getpass("Password: ")
 
-    # Check if the credentials are valid trying to login in the intranet
+    # Check if the credentials are valid by trying to login in the intranet
     print("Checking if the intranet username and password are valid")
     with sync_playwright() as playwright:
         chromium = playwright.chromium
@@ -91,17 +78,24 @@ def get_credentials():
 
 
 def install_script():
-    install_dir = "/usr/local/reservation_bot"
     script_dir = os.path.dirname(os.path.abspath(__file__))
     root_dir = os.path.dirname(script_dir)
 
-    print(root_dir)
     if platform.system() == "Darwin":
+        install_dir = "/usr/local/reservation_bot"
+
+        if os.path.exists(install_dir):
+            run_command(["sudo", "rm", "-rf", install_dir])
+
+        run_command(["sudo", "cp", "-r", f"{root_dir}/bot", install_dir])
+
+    if platform.system() == "Windows":
+        install_dir = "C:\\Program Files (x86)\\reservation_bot"
+
         if os.path.exists(install_dir):
             shutil.rmtree(install_dir)
 
-        print("Installing script...")
-        shutil.copytree(root_dir, install_dir)
+        shutil.copytree(f"{root_dir}\\bot", install_dir)
 
 
 if __name__ == "__main__":
