c3b699214b14e3697f8e7cff338517e55451515a
Joanjajas <joanbeltranperis@gmail.com>
2023-10-24 12:50:57 +0200

    sdhfe

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,85 +5,6 @@ root_dir=$(dirname "$scripts_dir")
 plist_file="$root_dir/reservation_bot.plist"
 install_dir="/usr/local/reservation_bot"
 
-main() {
-	install_deps
-	get_credentials
-	check_credentials
-	install_script
-	replace_credentials
-	create_dirs
-	create_launch_agent
-}
-
-# Install the necessary python dependencies
-install_deps() {
-	printf "\nInstalling dependencies...\n"
-	/usr/bin/python3 -m pip install playwright >/dev/null
-	/usr/bin/python3 -m playwright install chromium >/dev/null
-}
-
-# Prompt for username and password
-get_credentials() {
-	printf "Introduce your intranet username and password:\n"
-	printf "Dni: "
-	read -r username
-	printf "Password: "
-	stty -echo
-	read -r password
-	stty echo
-}
-
-# Check that the credentials are correct
-check_credentials() {
-	while true; do
-		printf "\nChecking if the intranet username and password are valid...\n"
-
-		credentials_script="$scripts_dir/check_credentials.py"
-
-		if /usr/bin/python3 "$credentials_script" "$username" "$password"; then
-			break
-		else
-			printf "The intranet username or password are not valid.\n\n"
-			get_credentials
-		fi
-	done
-}
-
-# Install the script in the system
-install_script() {
-	if [ -d /usr/local/reservation_bot ]; then
-		sudo rm -rf "$install_dir"
-	fi
-	sudo cp -r "$root_dir/bot" "$install_dir"
-}
-
-# Replace the credentials in the script
-replace_credentials() {
-	sudo sed -i '' "s/USERNAME = \".*\"/USERNAME = \"$username\"/" "$install_dir/bot.py"
-	sudo sed -i '' "s/PASSWORD = \".*\"/PASSWORD = \"$password\"/" "$install_dir/bot.py"
-}
-
-# Create the necessary directories and files
-create_dirs() {
-	if [ ! -d ~/bot_reservas ]; then
-		mkdir ~/bot_reservas
-	fi
-
-	if [ ! -f ~/bot_reservas/reservas.toml ]; then
-		cp "$root_dir/reservas.toml" ~/bot_reservas/
-	fi
-}
-
-# Create the launch agent
-create_launch_agent() {
-	if [ -f ~/Library/LaunchAgents/reservation_bot.plist ]; then
-		launchctl unload ~/Library/LaunchAgents/reservation_bot.plist
-	fi
-
-	sudo cp "$plist_file" ~/Library/LaunchAgents/
-	launchctl load ~/Library/LaunchAgents/reservation_bot.plist
-}
-
 # Prompt for the sudo password
 sudo -v
 
@@ -95,4 +16,54 @@ if ! ping -q -c 2 -W 2 google.com >/dev/null 2>&1; then
 	exit 1
 fi
 
-main
+# Install the necessary python dependencies
+printf "Installing dependencies...\n"
+/usr/bin/python3 -m pip install toml >/dev/null
+/usr/bin/python3 -m pip install playwright >/dev/null
+/usr/bin/python3 -m playwright install chromium >/dev/null
+
+# Get user credentials
+printf "\nIntroduce your intranet username and password:\n"
+printf "Dni: "
+read -r username
+printf "Password: "
+stty -echo
+read -r password
+stty echo
+
+printf "\nChecking if the intranet username and password are valid...\n"
+credentials_script="$scripts_dir/check_credentials.py"
+
+if ! /usr/bin/python3 "$credentials_script" "$username" "$password"; then
+	printf "The intranet username or password are not valid.\n"
+	printf "Please, try again.\n"
+	exit 1
+fi
+
+# Install the script in the system
+if [ -d /usr/local/reservation_bot ]; then
+	sudo rm -rf "$install_dir"
+fi
+sudo cp -r "$root_dir/bot" "$install_dir"
+
+# Replace the credentials inside the script
+sudo sed -i '' "s/USERNAME = \".*\"/USERNAME = \"$username\"/" "$install_dir/bot.py"
+sudo sed -i '' "s/PASSWORD = \".*\"/PASSWORD = \"$password\"/" "$install_dir/bot.py"
+
+# Create the necessary directories and files
+if [ ! -d ~/bot_reservas ]; then
+	mkdir ~/bot_reservas
+fi
+
+if [ ! -f ~/bot_reservas/reservas.toml ]; then
+	cp "$root_dir/reservas.toml" ~/bot_reservas/
+fi
+
+# Create the launch agent
+if [ -f ~/Library/LaunchAgents/reservation_bot.plist ]; then
+	launchctl unload ~/Library/LaunchAgents/reservation_bot.plist
+fi
+sudo cp "$plist_file" ~/Library/LaunchAgents/
+launchctl load ~/Library/LaunchAgents/reservation_bot.plist
+
+printf "\nSetup completed successfully.\n"
